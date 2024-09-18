2abc36dbe9b9daac70230361c538a0ed50b8c09a
Aayush07 <Aayush07@gmail.com>
2024-09-18 21:12:05 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+ import Navbar from './Component/Navbar/Navbar'
+import Hero from './Component/Hero/Hero'
+import Program from './Component/Program/Program'
+
+
+function App() {
+ 
+  return (
+<>
+<Navbar></Navbar>
+<Hero></Hero>
+
+
+<div className='.h1'>
+<Program></Program>
+</div>
+
+
+
+</>
+  )
+}
+
+export default App
---
+++ src/Component/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+   
+   <div className='h1'>
+ 
+<div className="hero-text">
+
+<h1 className='one'> Lorem ipsum dolor sit amet consectetur adipisicing elit. officia!</h1>
+
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam perferendis amet impedit recusandae consequuntur officiis suscipit exercitationem quisquam earum nostrum porro, similique cupiditate dicta, hic beatae alias expedita reprehenderit minima.</p>
+<button className='btn'>Explore more</button>
+
+
+
+</div>
+
+   </div>
+
+   
+
+  )
+}
+
+export default Hero
